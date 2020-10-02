0d06d8574520f7cfc480043683836c3c017c79eb
Altamarin <altamarin02212000@gmail.com>
2020-10-02 17:35:50 +0700

    update form

--- kuropotret/app/Http/Controllers/PriceController.php
+++ kuropotret/app/Http/Controllers/PriceController.php
@@ -13,7 +13,7 @@ class PriceController extends Controller
      */
     public function index()
     {
-        return view("pages.pricing.prewed");
+        return view("pages.pricing");
     }
 
     /**
--- kuropotret/resources/views/pages/pricing/prewed.blade.php
+++ kuropotret/resources/views/pages/pricing/prewed.blade.php
@@ -9,40 +9,52 @@
             <div class="col-lg-8">
                 <form class="form-contact contact_form" action="contact_process.php" method="post" id="contactForm" novalidate="novalidate">
                     <div class="row">
-                        <div class="input-group-icon mt-10">
-                            <div class="icon"><i class="fa fa-plane" aria-hidden="true"></i></div>
+
+                        <div class="col-12">
+                        <div class="form-group">
+                            <label for="Paket">Paket</label>
                             <div class="form-select" id="default-select">
-                                <select>
-                                    <option value=" 1">City</option>
-                                    <option value="1">Dhaka</option>
-                                    <option value="1">Dilli</option>
-                                    <option value="1">Newyork</option>
-                                    <option value="1">Islamabad</option>
+                                <select class="form-control">
+                                    <option value="Prewedding">Prewedding</option>
+                                    <option value="Akad">Akad</option>
+                                    <option value="Resepsi">Resepsi</option>
+                                    <option value="Music Video">Music Video</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <input class="form-control" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" placeholder='Enter your name'>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <input class="form-control" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" placeholder='Enter email address'>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="form-group">
-                                <input class="form-control" name="subject" id="subject" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Subject'" placeholder='Enter Subject'>
-                            </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                        <label for="Nama">Nama</label>
+                            <input class="form-control" name="name" id="name" type="text" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Your Name'" placeholder='Enter Your Name'>
                         </div>
                     </div>
-                    <div class="form-group mt-3">
-                        <button type="submit" class="button-contactForm btn_2">Send Message </button>
+                    <div class="col-12">
+                        <div class="form-group">
+                        <label for="Email">Email</label>
+                            <input class="form-control" name="email" id="email" type="email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email Address'" placeholder='Enter Email Address'>
+                        </div>
                     </div>
-                </form>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="Alamat">Alamat</label>
+                            <input class="form-control" name="alamat" id="alamat" type="alamat" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Address'" placeholder='Enter Address'>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="hp">Nomor Handphone</label>
+                            <input class="form-control" name="hp" id="hp" type="hp" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Phone Number'" placeholder='Enter Phone Number'>
+                        </div>
+                    </div>
             </div>
+            <div class="form-group mt-3">
+                <button type="submit" class="button-contactForm btn_2">Send Message </button>
+            </div>
+
+            </form>
         </div>
+    </div>
     </div>
 </section>
 @endsection
